a0f140330242b21aa4b24e536cfb6d01fb76f0c9
Eversmile Muthanuni <muthanunie@gmail.com>
2021-09-24 23:45:08 +0200

    new hello world function added

--- Team5/Team5/Test1.vb
+++ Team5/Team5/Test1.vb
@@ -2,4 +2,8 @@
     Public Shared Function hello() As String
         Return "Hello world"
     End Function
+
+    Public Shared Function newHello() As String
+        Return "Hello world II"
+    End Function
 End Class
