f44a9233dbd7fd1577848e530f03211992b8d610
Eversmile Muthanuni <muthanunie@gmail.com>
2021-09-25 16:13:50 +0200

    added btn3

--- Team5/Team5/Form1.vb
+++ Team5/Team5/Form1.vb
@@ -1,9 +1,14 @@
 ﻿Public Class Form1
+    'Yoli
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         MessageBox.Show(Test1.hello())
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
         MessageBox.Show(Test1.newHello())
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show(Test2.sum())
     End Sub
 End Class
---
+++ Team5/Team5/Test2.vb
@@ -0,0 +1,6 @@
+﻿Public Class Test2
+
+    Public Shared Function sum() As Integer
+        Return 1 + 1
+    End Function
+End Class
