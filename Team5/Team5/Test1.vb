5ef06bf2b2aebe957c6d76aaa6d1948dcd62bc74
Eversmile Muthanuni <muthanunie@gmail.com>
2021-09-25 16:33:01 +0200

    clean start up

--- Team5/Team5/Test1.vb
+++
@@ -1,9 +0,0 @@
-﻿Public Class Test1
-    Public Shared Function hello() As String
-        Return "Hello world"
-    End Function
-
-    Public Shared Function newHello() As String
-        Return "Hello world II"
-    End Function
-End Class
--- Team5/Team5/Test2.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Test2
-
-    Public Shared Function sum() As Integer
-        Return 1 + 2
-    End Function
-End Class
