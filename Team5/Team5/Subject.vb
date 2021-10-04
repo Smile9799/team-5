f8d28c8ecdc65a2a0315a1cb45c5e68f62e22ac6
Thando-Qondelani <tandoqondelani@gmail.com>
2021-10-05 00:25:36 +0200

    changed test2 class

--- Team5/Team5/Subject.vb
+++ Team5/Team5/Subject.vb
@@ -1,21 +1,10 @@
 ﻿Option Strict On
 Option Explicit On
 Option Infer Off
-<<<<<<< HEAD
-Public Class Subject
-    Private _SubjectName As String
-    Private _DificultyRating As Integer
-
-    Public Sub New(subName As String, Dif As Integer)
-        _SubjectName = subName
-        _DificultyRating = Dif
-    End Sub
-=======
 <Serializable()> Public Class Subject
 
     Private _SubjectName As String
     Private _DificultyRating As Integer
->>>>>>> 519c84ab084146de9bd7a0c09d214094f900226e
 
     'constructor
     Public Sub New(Name As String, rating As Integer)
