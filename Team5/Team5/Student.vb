a95c2b5096ef34122eb42dfb853cebe0cdd59a79
Yoliswa-Qithi <yoyoqithi@gmail.com>
2021-10-05 20:32:53 +0200

    changes to word document Team 5

--- Team5/Team5/Student.vb
+++ Team5/Team5/Student.vb
@@ -8,6 +8,7 @@ Public Class Student
     Private _StudentID As String
     Private _TransportNeeded As Boolean
     Private _DistanceFromSchool As Integer
+    Private _AmountForTransport As Double
 
     Public Sub New(Name As String)
         MyBase.New(Name)
@@ -48,12 +49,42 @@ Public Class Student
             Return _DistanceFromSchool
         End Get
         Set(value As Integer)
-            _DistanceFromSchool = value
+            _DistanceFromSchool = Validate(value)
+        End Set
+    End Property
+
+    Public Property AmountForTransport As Double
+        Get
+            Return _AmountForTransport
+        End Get
+        Set(value As Double)
+            _AmountForTransport = value
         End Set
     End Property
 
     Public Function calcTripFare() As Double
-        Dim Price As Double
-        Return Price * DistanceFromSchool
+
+        Return AmountForTransport * DistanceFromSchool
+    End Function
+
+    Public Function Validate() As Integer
+        If DistanceFromSchool < 0 Then
+            _DistanceFromSchool = 0
+        Else
+            _DistanceFromSchool = value
+        End If
+    End Function
+
+    Public Overrides Function Display() As String
+        Dim Ans As String = environment.newline & MyBase.Display
+        Ans &= "Is Attending :" & _IsAttending & Environment.newline
+        Ans &= "Has Passed :" & _HasPassed & Environment.newline
+        Ans &= "Student ID :" & _StudentID & Environment.newline
+        Ans &= "Transport Needed :" & _TransportNeeded & Environment.newline
+        Ans &= "Distance From School :" & _DistanceFromSchool & Environment.newline
+        Ans &= "Amount for Transport :" & _AmountForTransport & Environment.newline
+        Ans &= "Calulation For Trip Fare :" & calcTripFare() & environment.newline
+
+        Return Ans
     End Function
 End Class
